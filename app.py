8dfdd69fd80bde55bcaf558467d5d0cb6cf27cce
Esteban190904 <wurttelee@gmail.com>
2025-02-10 17:35:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,7 @@ client = OpenAI(api_key = st.secrets.OpenAIAPI.openai_api_key)
 sidebar = st.sidebar
 
 page = sidebar.radio("Seleccione Chatbot", ["Trip Adviser", "Bussines adviser"])
+  if page == "Trip adviser":
+    Page1()
+  elif page == "Business Adviser":
+    st.session_state["Business_adviser_message"] = []

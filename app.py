c5c52f2ca35ff18668007dd228a15a8e8f18ca69
Esteban190904 <wurttelee@gmail.com>
2025-02-10 17:28:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,17 +3,12 @@ from openai import OpenAI
 
 client = OpenAI(api_key = st.secrets.OpenAIAPI.openai_api_key)
 
-st.title("Trip adviser AI")
-st.write("Utilizing the ChatGPT API, this chatbot offers advanced conversational capabilities.")
+def main():
+  sidebar = st.sidebar
 
-user_input = st.text_input("Please enter a message here", key = "user_input", on_change=comunicate)
+  page = sidebar.radio("Seleccione Chatbot", ["Trip Adviser", "Bussines adviser"])
 
-if st.session_state["message"]:
-  message = st.session_state["message"]
-
-  for message in reversed(message[1:]):
-    if isinstance(message, dict):
-      speaker = "😎" if message["role"] == "user" else "🙄"
-      st.write (speaker + ": " + message["content"])
-    else:
-      st.write("😮: " + message.content)
+  if page == "Trip adviser":
+    Page1()
+  elif page == "Business Adviser":
+    st.session_state["Business_adviser_message"] = []

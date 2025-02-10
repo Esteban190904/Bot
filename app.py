0acfa7638c5388418bfeb2b4062f9958c887e8e5
Esteban190904 <wurttelee@gmail.com>
2025-02-10 17:41:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -41,25 +41,5 @@ class Page2:
       ]
 
 
-    def communicate():
-
-      
-      client = OpenAI(api_key = st.secrets.OpenAIAPI.openai_api_key)
-
-      if "messages" not in st.session_state:
-        st.session_state["messages"] = [  
-          {"role":"system", "content": prompt}
-        ]
-      messages = st.session_state["messages"]
-      user_message = {"role":"user", "content": st.session_state["user_input"]}
-      messages.append(user_message)
-
-      response = client.chat.completions.create(
-          model="gpt-3.5-turbo",
-          messages=messages
-      )
-
-      bot_message = response.choices[0].message
-      messages.append(bot_message)
-
+ 
 

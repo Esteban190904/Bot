0d7d0dbd45c157a5663611135e155839b0b7db2f
Esteban190904 <wurttelee@gmail.com>
2025-02-10 17:31:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,5 @@ from openai import OpenAI
 client = OpenAI(api_key = st.secrets.OpenAIAPI.openai_api_key)
 
 st.sidebar.title ("sidebar")
-st.sidebar ("Hola MUNDOO")
+y = st.sidebar.slider ("El valor de y ")
+st.sidebar.write ("El cuadrado de " + str(y) + " es " + str(y**2))
